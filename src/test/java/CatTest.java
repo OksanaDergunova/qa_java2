130c3a1887c15a29c1ef38cc124062a269f82f32
Дарья <gozenko.dasha@bk.ru>
2025-09-10 20:37:58 +0400

    result commit

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,36 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatTest {
+
+    private Cat cat;
+    private Feline feline;
+
+    @Before
+    public void setUp(){
+        feline = new Feline();
+        cat = new Cat(feline);
+    }
+
+    @Test
+    public void testGetSound(){
+        assertEquals("Мяу",cat.getSound());
+    }
+
+    @Test
+    public void testGetFood(){
+       try{
+        List<String> catFood =  cat.getFood();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"),catFood);
+       }catch (Exception e){
+           throw  new RuntimeException();
+       }
+    }
+
+}
